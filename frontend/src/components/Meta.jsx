38f0b50db81870f3f79daf17d5a14618f5cbb118
Ryan Batchelor <ryanjbatch@gmail.com>
2023-08-31 16:42:01 -0500

    Upload

---
+++ frontend/src/components/Meta.jsx
@@ -0,0 +1,19 @@
+import { Helmet } from 'react-helmet-async';
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  );
+};
+
+Meta.defaultProps = {
+  title: '279 Construction Supplies',
+  description: 'We sell construction supplies',
+  keywords: 'All your construction needs',
+};
+
+export default Meta;
